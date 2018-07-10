d7e22b7247e9d1a4f0a2b784d18dda678acc3d09
Luciano Baraúna <baraunaluciano@gmail.com>
2018-07-10 01:38:11 +0000

    update vue-cli

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div(id="app")
+    GradientButton(
+      posTop="-10px",
+      posLeft="-10px",
+      posBotom="-10px",
+      posRight="-10px",
+      radiusSize="50px",
+      gradientBorder= "linear-gradient(45deg, #ff0, #f0f, #f00, #00f, #0f0)"
+    ) Lorem ipsum
+</template>
+
+<script>
+import GradientButton from './components/GradientButton/GradientButton'
+
+export default {
+  name: 'app',
+  components: {
+    GradientButton
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/sass/v-gradientbutton'
+</style>
---
+++ src/components/GradientButton/GradientButton.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  button(
+    :class='customClass'
+    class='gradient-button'
+    )
+    div(
+      class="gradient-button-wrap"
+      :style="{ borderRadius: radiusSize, backgroundImage: gradientBorder, bottom: posBotom, left: posLeft, right: posRight, top: posTop}"
+    )
+    span.text
+      slot
+
+</template>
+
+<script>
+export default {
+  name: 'GradientButton',
+  data () {
+    return {
+      classButton: 'gradient-button'
+    }
+  },
+  computed: {
+  },
+  props: {
+    customClass: {
+      type: String,
+      default: function () {
+        return 'default'
+      }
+    },
+    radiusSize: {
+      type: String,
+      required: true
+    },
+    gradientBorder: {
+      type: String,
+      required: true
+    },
+    posTop: {
+      type: String,
+      required: true
+    },
+    posLeft: {
+      type: String,
+      required: true
+    },
+    posBotom: {
+      type: String,
+      required: true
+    },
+    posRight: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import './../../assets/sass/import/_extends.sass'
+  @import './GradientButton.sass'
+</style>
